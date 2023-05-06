ca372d8c31e3f6300819a0037041d2cb3f100ef8
Cesar Acosta <cesar.acosta@gmail.com>
2023-05-06 14:02:40 -0400

    integrand flavor

---
+++ lib/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'themes/my_theme.dart';
+
+import 'flavors/flavors.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: F.title,
+      theme: MyTheme.lightTheme,
+      home: _flavorBanner(
+        child: MyHomePage(title: F.title,),
+        show: kDebugMode,
+      ),
+    );
+  }
+
+  Widget _flavorBanner({
+    required Widget child,
+    bool show = true,
+  }) =>
+      show
+          ? Banner(
+        location: BannerLocation.topStart,
+        message: F.name,
+        color: Colors.green.withOpacity(0.6),
+        textStyle: const TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 12.0,
+            letterSpacing: 1.0),
+        textDirection: TextDirection.ltr,
+        child: child,
+      )
+          : Container(
+        child: child,
+      );
+}
